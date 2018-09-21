04c3da5b37aa83b836e9db4bf8da77d424753b8b
JM <jessmorrison90@gmail.com>
2018-09-21 18:25:36 -0500

    someval
    super close, still getting NaN

--- script.js
+++ script.js
@@ -1,94 +1,64 @@
 (function () {
-    "use strict";
-    window.onload = function () {
-        //Add event listener here!	
-        var button = document.getElementById('btn');
-        button.addEventListener('click', AddRow);
+        "use strict";
+        window.onload = function () {
+            //Add event listener here!	
+            var button = document.getElementById('btn');
+            button.addEventListener('click', AddRow);
 
-        function AddRow() {
-            var list = document.getElementById("list");
-            var row = document.createElement("tr");
-            var cell = document.createElement("td");
-            var text = document.createTextNode("added");
-            
-            var name = document.getElementById("name").value;
-            var price = document.getElementById("price").value;
-            
-            
-            
-            cell.appendChild(text);
-            row.appendChild(cell);
-            list.appendChild(row);
-            
-            CreateInnerHTML(name, price, row);
-           
-        }
-        
-        function CreateInnerHTML(name, price, row) {
-            
-            var cell2 = document.createElement("td");
-            var cell3 = document.createElement("td");
+            function AddRow() {
+                var list = document.getElementById("list");
+                var row = document.createElement("tr");
+                var cell = document.createElement("td");
+                var text = document.createTextNode("added");
 
-            var text2 = document.createTextNode(name);
-            var text3 = document.createTextNode(price);
+                var name = document.getElementById("name").value;
+                var price = document.getElementById("price").value;
 
-            cell2.appendChild(text2);
-            row.appendChild(cell2);
-            list.appendChild(row);
+                cell.appendChild(text);
+                row.appendChild(cell);
+                list.appendChild(row);
 
-            cell3.appendChild(text3);
-            row.appendChild(cell3);
-            list.appendChild(row);
-            
-        }
-        
-        function UpdateTotal (totalCost) {
-            
-            var totalT = parseFloat(totalCost);
-            return ("$" + totalT);
-            
-            
-        }
+                CreateInnerHTML(name, price, row);
+                UpdateTotal();
 
+            }
 
+            function CreateInnerHTML(name, price, row) {
+             
+                var cell2 = document.createElement("td");
+                var cell3 = document.createElement("td");
 
-       
+                var text2 = document.createTextNode(name);
+                var text3 = document.createTextNode(price);
 
+                cell2.appendChild(text2);
+                row.appendChild(cell2);
+                
+                cell3.appendChild(text3);
+                row.appendChild(cell3);
+             
+            }
 
+            function UpdateTotal() {
 
+                var table = document.getElementById("table"),
+                    sumVal = 0;
+               
+               
 
-
-
-
-        /* WORKING CODE BEFORE ABSTRACTION START
-                function AddRow() {
-
-                    var list = document.getElementById("list");
-                    var row = document.createElement("tr");
+                for (var i = 1; i < table.rows.length; i++) {
+                    alert(sumVal);
                     
-                    var cell = document.createElement("td");
-                    var cell2 = document.createElement("td");
-                    var cell3 = document.createElement("td");
+                    sumVal = sumVal + parseInt(table.rows[i].cells[2].innerHTML);
+                }
+                if (isNaN(sumVal)) {
+                    sumVal= parseFloat(sumVal);
+                }
+               
+                document.getElementById("totalPrice").innerHTML = sumVal;
+            }
+        }
+    }
+    ());
 
-                    var name = document.getElementById("name").value;
-                    var price = document.getElementById("price").value;
 
-                    var text = document.createTextNode("added");
-                    var text2 = document.createTextNode(name);
-                    var text3 = document.createTextNode(price);
-
-                    cell.appendChild(text);
-                    row.appendChild(cell);
-                    list.appendChild(row);
-
-                    cell2.appendChild(text2);
-                    row.appendChild(cell2);
-                    list.appendChild(row);
-
-                    cell3.appendChild(text3);
-                    row.appendChild(cell3);
-                    list.appendChild(row);
-        WORKING CODE BEFORE ABSTRACTION END */
-
-    };
-}());
