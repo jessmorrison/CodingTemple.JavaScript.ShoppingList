9da9d49b3067fad4ef196654ada3bc7dd0778ad7
JM <jessmorrison90@gmail.com>
2018-09-21 19:10:24 -0500

    9/21/18 - Completed Project
    7:10pm - project complete and functioning as instructed

--- script.js
+++ script.js
@@ -13,6 +13,13 @@
 
                 var name = document.getElementById("name").value;
                 var price = document.getElementById("price").value;
+                
+                /* Validation check */
+                
+                if (name == "" || price == "") {
+                    alert("all fields must have input");
+                    return false;
+                }
 
                 cell.appendChild(text);
                 row.appendChild(cell);
@@ -20,11 +27,10 @@
 
                 CreateInnerHTML(name, price, row);
                 UpdateTotal();
-
             }
 
             function CreateInnerHTML(name, price, row) {
-             
+
                 var cell2 = document.createElement("td");
                 var cell3 = document.createElement("td");
 
@@ -33,32 +39,21 @@
 
                 cell2.appendChild(text2);
                 row.appendChild(cell2);
-                
+
                 cell3.appendChild(text3);
                 row.appendChild(cell3);
-             
+
             }
 
             function UpdateTotal() {
 
                 var table = document.getElementById("table"),
                     sumVal = 0;
-               
-               
-
-                for (var i = 1; i < table.rows.length; i++) {
-                    alert(sumVal);
-                    
+                for (var i = 1; i < table.rows.length-1; i++) {
                     sumVal = sumVal + parseInt(table.rows[i].cells[2].innerHTML);
+                    document.getElementById("totalPrice").innerHTML = sumVal;
                 }
-                if (isNaN(sumVal)) {
-                    sumVal= parseFloat(sumVal);
-                }
-               
-                document.getElementById("totalPrice").innerHTML = sumVal;
             }
         }
     }
     ());
-
-
